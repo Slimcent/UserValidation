c99795388283443d59d42bda7d7edc56467df90a
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-12-17 16:57:23 +0100

    email validation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from email_validator import validate_email, EmailNotValidError, caching_resolver
+
 
 # while first_name == '':
 #     print("first number cannot be empty")
@@ -18,9 +20,35 @@ def validate_name(name):
     return name
 
 
-first_name = input("Enter first name")
+def check_email(user_email):
+    while True:
+        # resolver = caching_resolver(timeout=10)
+        if user_email.strip() == '':
+            print("email cannot be empty")
+            user_email = input(user_email)
+        else:
+            try:
+                user_email = validate_email(user_email).email
+            except EmailNotValidError as e:
+                print(str(e))
+                user_email = input(user_email)
+                continue
+            else:
+                return user_email
+                break
+
+
+email = input("Enter email")
+email = input(email)
+email = check_email(email)
+print(f"Your email is {email}")
+
+last_name = input("Enter last name")
+last_name = str(input(last_name))
+last_name = validate_name(last_name)
+print(f"First name is {last_name}")
+
+first_name = input("enter first name")
 first_name = str(input(first_name))
 first_name = validate_name(first_name)
-
-
 print(f"First name is {first_name}")
